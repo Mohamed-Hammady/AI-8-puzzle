893ee6cf1222f3cb383fe45972662d57dfa8b135
Mohamed-Hammady <mohamedehab18127@gmail.com>
2024-10-27 19:42:35 +0300

    improve gui

--- main.py
+++ main.py
@@ -7,7 +7,15 @@ def update_puzzle():
     state_list = number_to_state(state)
     for i in range(3):
         for j in range(3):
-            puzzle_grid[i][j].config(text=state_list[i][j])
+            if not state_list[i][j] == "0":
+                puzzle_grid[i][j].config(text=state_list[i][j], bg="grey", fg="white",
+                                         width=8, height=4, bd=4, relief="solid",
+                                         highlightbackground="black", highlightthickness=3, font=("Helvetica", 24))
+            else:
+                puzzle_grid[i][j].config(text=state_list[i][j], bg="black", fg="black",
+                                         width=8, height=4, bd=4, relief="flat",
+                                         highlightbackground="black", highlightthickness=3, font=("Helvetica", 24))
+
 
 
 def next_state():
@@ -31,6 +39,7 @@ while True:
         start = input()
     if start == "exit":
         break
+
     initial_state = []
     while not enter_state(initial_state):
         print("invalid input, please enter the game in that form with different order\n0 1 2\n3 4 5\n6 7 8")
@@ -38,6 +47,7 @@ while True:
     if not solvable(initial_state):
         print("The puzzle can't be solved")
         continue
+
     choice = False
     child = {}
     parent = {}
@@ -59,9 +69,17 @@ while True:
                 print("Euclidean A*")
             case _:
                 choice = False
+
     root = tk.Tk()
     root.title("8-Puzzle Game")
-    root.geometry("250x250")
+
+    window_width = 380
+    window_height = 450
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
+    position_x = (screen_width // 2) - (window_width // 2)
+    position_y = (screen_height // 2) - (window_height // 2)
+    root.geometry(f'{window_width}x{window_height}+{position_x}+{position_y}')
 
     state = initial_state
     initial_state = number_to_state(initial_state)
@@ -69,13 +87,21 @@ while True:
     for i in range(3):
         row = []
         for j in range(3):
-            label = tk.Label(root, text=initial_state[i][j], width=5, height=2, font=("Helvetica", 24))
+            if not initial_state[i][j] == "0":
+                label = tk.Label(root, text=initial_state[i][j], bg="grey",
+                                 width=8, height=4, bd=4, relief="solid",
+                                 highlightbackground="black", highlightthickness=3, font=("Helvetica", 24))
+            else:
+                label = tk.Label(root, text=initial_state[i][j], fg="black", bg="black", width=8, height=4,
+                                 bd=4, highlightbackground="black", highlightthickness=3, font=("Helvetica", 24))
             label.grid(row=i, column=j)
             row.append(label)
         puzzle_grid.append(row)
 
-    prev_button = tk.Button(root, text="Prev", command=prev_state)
-    next_button = tk.Button(root, text="Next", command=next_state)
+    prev_button = tk.Button(root, text="<--", width=6, height=2,
+                            bd=3, font=("Helvetica", 24), command=prev_state)
+    next_button = tk.Button(root, text="-->", width=6, height=2,
+                            bd=3, font=("Helvetica", 24), command=next_state)
     prev_button.grid(row=4, column=0)
     next_button.grid(row=4, column=2)
 
